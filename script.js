735d23d3e11c0d9259fae6f54fb039673d5a4fbd
AliAhmed0099 <syborrg99899@gmail.com>
2026-01-24 03:02:35 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+var tl=gsap.timeline(); 
+tl.from(".line h1",{
+    y:140,
+    stagger:0.25,
+    duration:0.75,
+    delay:0.6,
+}) 
+
+tl.from("#line-part1",{
+opacity:0,
+onStart:function(){
+var h5=document.querySelector("#line-part1 h5");
+var grow=0;
+setInterval(function(){
+    if(grow<100){
+ h5.innerHTML=grow++;
+    }
+    else{
+        h5.innerHTML=grow;
+    }
+},33);
+}
+});
+
+tl.to(".line h2",{      
+    animationName:"anime",
+    opacity:1,
+    
+})
+
+tl.to("#loader",{
+    opacity:0,
+    duration:0.2,
+    delay:3,
+})
+
+tl.from("#page1",{
+    y:1600,
+    opacity:0,
+    delay:0.2,
+    duration:0.5,   
+    ease:Power4,    
+})
+
+tl.to("#loader",{
+     display:"none",
+})
